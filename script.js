a261a49c70a6a0a617a2f83503bb3a1b8cd6c2e6
mariannefabre <marianne.fabre@hotmail.fr>
2021-05-15 17:49:10 +0200

    add sideNav

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// side menu
-
-let sideNav = document.getElementById("nav-items");
-
-const openSideNav = function () {
-  function isNavOpen() {
-    return sideNav.style.width === "100%" ? true : false;
-  }
-  function openNav() {
-    sideNav.style.width = "100%";
-  }
-  function closeNav() {
-    sideNav.style.width = "0";
-  }
-
-  document.getElementById("nav-icon").classList.toggle("open");
-  isNavOpen() ? closeNav() : openNav();
-};
-
-sideNav.addEventListener("click", openSideNav, true);
-
-document
-  .getElementById("nav-icon")
-  .addEventListener("click", openSideNav, true);
-
 // path
 
 document.getElementById("path").setAttribute("d", "M2,5 S2,14 4,5 S7,8 8,4");
